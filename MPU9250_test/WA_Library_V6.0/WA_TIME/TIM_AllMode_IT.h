af7830629a7cda8053b899d2a0ccedf2d245d0a3
gupid <ywq2640780145@163.com>
2025-07-10 01:04:04 +0800

    remove unwanted folders

--- MPU9250_test/WA_Library_V6.0/WA_TIME/TIM_AllMode_IT.h
+++
@@ -1,8 +0,0 @@
-#ifndef __TIM_ALLMODE_IT_H
-#define __TIM_ALLMODE_IT_H
-
-#include "main.h"
-
-
-
-#endif
--- MPU9250_test/WA_Library_V6.0/WA_UART/UART_IT.h
+++
@@ -1,9 +0,0 @@
-#ifndef __UART_IT_H
-#define __UART_IT_H
-
-
-#include "main.h"
-
-
-
-#endif
